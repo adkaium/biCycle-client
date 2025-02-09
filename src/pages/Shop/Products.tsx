a01e6f4ba956e8441af0729f87345ae4dd37e1cf
Ad Kaium <adkaiumalikhan01@gmail.com>
2025-02-09 22:55:25 +0300

    add all

--- src/pages/Shop/Products.tsx
+++ src/pages/Shop/Products.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+
 
 export const Products = () => {
   return (
     <section className='section__container product__container'>
-        
+        <h1 className="section__header">Trending Product</h1>
+        <p className="section__subheader mb-12">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos nemo neque quibusdam ratione ab, error ullam mollitia provident?</p>
     </section>
   )
 }
