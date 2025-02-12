174bd2b3d59a9687396d9214610bbd343d4a034d
Ad Kaium <adkaiumalikhan01@gmail.com>
2025-02-12 16:05:33 +0300

    create section

--- src/pages/home/DealsSection.tsx
+++ src/pages/home/DealsSection.tsx
@@ -1,10 +1,23 @@
-
-
+import dealsImg from "../../assets/deals.png";
 const DealsSection = () => {
   return (
-    <div
-    className="section__container deals__container">DealsSection</div>
-  )
-}
+    <section className="section__container deals__container">
+      DealsSection
+      <div className="deals__image">
+        <img src={dealsImg} alt="" />
+      </div>
+      <div className="deals__content">
+        <h5>Get Up To 20% Discount</h5>
+        <h4>Deals Of The Month</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          maiores est soluta quod esse incidunt veniam a aperiam, laudantium
+          perferendis autem ducimus tempore voluptate? Recusandae voluptatem non
+          sequi deserunt similique?a
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default DealsSection
+export default DealsSection;
